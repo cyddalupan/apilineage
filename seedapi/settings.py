94c605898b4e47c53979ba1926d746ad96bd85ca
Cleddrence Dalupan <Cleddrence.Dalupan@intelligentreach.com>
2022-10-03 11:29:43 +0800

    allow all access

--- seedapi/settings.py
+++ seedapi/settings.py
@@ -28,10 +28,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",
-    "http://127.0.0.1:9000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:4200",
+#     "http://127.0.0.1:9000",
+# ]
+
+CORS_ORIGIN_ALLOW_ALL=True
 
 # Application definition
 
@@ -50,6 +52,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,8 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
 ]
 
 REST_FRAMEWORK = {
